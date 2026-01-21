e4f07a0247ec7247e12f2e4e193ea15238e26f40
Gelei Xu <453447505@qq.com>
2026-01-21 16:11:07 -0500

    Update Service, Awards

--- src/components/Service/ServiceItem.tsx
+++ src/components/Service/ServiceItem.tsx
@@ -5,10 +5,13 @@ interface ServiceItemProps {
     content: string;
 }
 
-// ServiceItem: displays a single service entry
 const ServiceItem: React.FC<ServiceItemProps> = ({ year, content }) => (
     <div className="text-gray-800">
-        <span className="inline-block bg-stone-200 text-stone-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">{year}</span>
+        {year && (
+            <span className="inline-block bg-stone-200 text-stone-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded whitespace-nowrap">
+                {year}
+            </span>
+        )}
         {content}
     </div>
 );
